2b51d6c087de900270205a0d06d2084d3fde7eb0
Joshua Smith <joshuasmith6556@gmail.com>
2022-01-12 11:56:14 -0800

    Complete conditional portion of lab

---
+++ conditional.py
@@ -0,0 +1,31 @@
+# Name: Joshua Smith
+# Section: 07
+
+# Purpose: Determine smaller of two numbers
+# Signature: float float -> float
+def min_of_2(first, second):
+   return first if first < second else second
+
+
+# Purpose: Determine smallest of three numbers
+# Signature: float float float -> float
+def min_of_3(first, second, third):
+   first_second_min = min_of_2(first, second)
+   second_third_min = min_of_2(second, third)
+
+   return min_of_2(first_second_min, second_third_min)
+
+
+# Purpose: Determine late fee based on days late
+# Signature: int -> int
+def rental_late_fee(days):
+   if days <= 2:
+      return 0
+   elif days <= 5:
+      return 10
+   elif days <= 12:
+      return 15
+   elif days <= 22:
+      return 30
+   else:
+      return 100
--- conditional_tests.py
+++ conditional_tests.py
@@ -6,13 +6,36 @@
 # Section: 07
 
 import unittest
+from math import e, pi
 from conditional import *
 
 #You can delete pass after wrinting your code
 class TestCases(unittest.TestCase):
    def test_min_of_2(self):
-      # Add code here.
-      pass
+      # Testing min_of_2 which determines smaller of two numbers
+      self.assertEqual(min_of_2(4, 6), 4)
+      self.assertEqual(min_of_2(-12, -24), -24)
+      self.assertAlmostEqual(min_of_2(e * pi, 60), 8.53973422267)
+
+   def test_min_of_3(self):
+      # Testing min_of_3 which determines smallest of three numbers
+      self.assertEqual(min_of_3(0, 3, 6), 0)
+      self.assertEqual(min_of_3(-124, 325, -124978), -124978),
+      self.assertEqual(min_of_3(0, 1, 0), 0)
+      self.assertEqual(min_of_3(-1, 0, 0), -1)
+      self.assertEqual(min_of_3(pi * e, 0, 14), 0)
+
+   def test_rental_late_fee(self):
+      # Testing rental_late_fee which determines late fee based on days late
+      self.assertEqual(rental_late_fee(0), 0)
+      self.assertEqual(rental_late_fee(2), 0)
+      self.assertEqual(rental_late_fee(3), 10)
+      self.assertEqual(rental_late_fee(5), 10)
+      self.assertEqual(rental_late_fee(6), 15)
+      self.assertEqual(rental_late_fee(12), 15)
+      self.assertEqual(rental_late_fee(13), 30)
+      self.assertEqual(rental_late_fee(22), 30)
+      self.assertEqual(rental_late_fee(23), 100)
 
 
 # Run the unit tests.

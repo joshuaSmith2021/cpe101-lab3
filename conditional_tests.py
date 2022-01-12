b90bcc4c36e42bcd7698248bb01d1af62a6629d0
Joshua Smith <joshuasmith6556@gmail.com>
2022-01-12 11:07:53 -0800

    Initial commit

---
+++ conditional_tests.py
@@ -0,0 +1,21 @@
+# This is a header block example for lab 03.
+
+# You will need to supply the following information.
+
+# Name:
+# Section:
+
+import unittest
+from conditional import *
+
+#You can delete pass after wrinting your code
+class TestCases(unittest.TestCase):
+   def test_min_of_2(self):
+      # Add code here.
+      pass
+
+
+# Run the unit tests.
+if __name__ == '__main__':
+   unittest.main()
+
---
+++ logic_tests.py
@@ -0,0 +1,22 @@
+
+# This is a header block example for lab 03.
+
+# You will need to supply the following information.
+
+# Name:
+# Section:
+
+import unittest
+from logic import *
+
+#You can dlete pass after wrinting your code
+class TestCases(unittest.TestCase):
+   def test_is_odd (self):
+      # Add code here.
+      pass
+
+
+# Run the unit tests.
+if __name__ == '__main__':
+   unittest.main()
+
